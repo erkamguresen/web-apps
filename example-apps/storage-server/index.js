8d244ba35abf9aed8b56fba5f75f26d839d14f50
ERKAM GURESEN <46300684+erkamguresen@users.noreply.github.com>
2021-09-11 11:21:46 +0200

    finished

--- example-apps/storage-server/index.js
+++ example-apps/storage-server/index.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const morgan = require('morgan');
 const fs = require('fs');
+const crypto = require('crypto');
 
 const { readData, writeData, deleteData } = require('./data-access');
 
@@ -16,6 +17,8 @@ const userDB = `${__dirname}/data/credentials.txt`;
 // in the format of ['name:password', 'name:password']
 const credentials = fs.readFileSync(userDB, 'utf-8').split('\n');
 
+const accessTokenPath = `${__dirname}/data/access-tokens.json`;
+
 // use parsing and logging middleware
 app.use(bodyParser.json());
 app.use(morgan('dev'));
@@ -23,19 +26,83 @@ app.use(morgan('dev'));
 // register new users
 app.post('/register', (req, res) => {
   const { username, password } = req.body;
-  const user = { username, password };
+  const user = { username, hashedPassword: hashPassword(password) };
 
   // check if user already exists
-  if (credentials.includes(`${username}:${password}`)) {
-    res.status(400).send('User already exists');
+  if (credentials.find((cred) => cred.split(':')[0] === username)) {
+    return res.status(400).send('User already exists');
   }
 
   // add user to credentials
   credentials.push(`${username}:${password}`);
+
+  // write credentials to file
+  fs.writeFileSync(userDB, credentials.join('\n'));
+
+  // send success response
+  res.status(201).send('User created'); // 201 = created
 });
+
 // login
+app.post('/login', (req, res) => {
+  const sentUserName = req.body.username;
+  const sentPassword = req.body.password;
+
+  // check if user exists
+  if (!credentials.includes(`${sentUserName}:${sentPassword}`)) {
+    return res.status(401).send('User not found');
+  }
+
+  // generate access token
+  const accessToken = crypto.randomBytes(64).toString('hex');
+
+  // add access token to access token file
+  const storedAccessTokens = JSON.parse(
+    fs.readFileSync(accessTokenPath, 'utf-8'),
+  );
+
+  storedAccessTokens[accessToken] = {
+    sentUserName,
+    createdAt: new Date(),
+    expiresIn: new Date(Date.now() + 1000 * 60 * 60 * 24),
+  };
+
+  fs.writeFileSync(accessTokenPath, JSON.stringify(storedAccessTokens));
+
+  console.log(storedAccessTokens);
+
+  // send success token
+  res.status(200).send({ secretToken: accessToken });
+});
 
 // middleware to check if user is logged in based on token
+app.use((req, res, next) => {
+  let token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).send('Unauthorized');
+  }
+
+  token = token.replace('Bearer ', '');
+
+  const storedAccessTokens = JSON.parse(
+    fs.readFileSync(accessTokenPath, 'utf-8'),
+  );
+
+  if (!storedAccessTokens[token]) {
+    return res.status(401).send('Unauthorized');
+  }
+
+  if (storedAccessTokens[token].expiresIn < new Date()) {
+    delete storedAccessTokens[token];
+    fs.writeFileSync(accessTokenPath, JSON.stringify(storedAccessTokens));
+    return res.status(401).send('Unauthorized');
+  }
+
+  // req.username = storedAccessTokens[token].sentUserName;
+
+  next();
+});
 
 // define routes
 app.get('/data/:ownerName', async (req, res) => {
@@ -87,5 +154,9 @@ app.listen(3000, (err) => {
     return;
   }
 
-  console.info('server started on port 3000');
+  console.info('server started on port 3000\n http://localhost:3000');
 });
+
+function hashPassword(input) {
+  return crypto.createHash('sha3-256').update(input).digest('hex');
+}
